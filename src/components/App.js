228400660a982f56168df002554e0b739800fb5d
Justin Byrd <justinbyrd7@gmail.com>
2023-10-05 14:40:01 -0500

    connected premade

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import { initialState, reducer } from "../reducers/index";
-import * as actions from "../actions/index";
+import { addOne } from "../actions/index";
 
 import "./App.css";
 
@@ -39,7 +39,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton value={1} onClick={() => dispatch(addOne())} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
